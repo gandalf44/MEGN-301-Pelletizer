077774bd0d34e6aebc76188c80620c01a25f7492
gandalf44 <57021826+gandalf44@users.noreply.github.com>
2023-03-06 11:51:44 -0700

    First Gen Threading test

--- Test_Code/Nathan/threadding/threadding.ino
+++ Test_Code/Nathan/threadding/threadding.ino
@@ -1,3 +1,6 @@
+// Include necessary libraries:
+#include <Thread.h>
+
 
 ///// PORTS /////
 
@@ -6,11 +9,29 @@
 #define RED 10
 #define YELLOW 11
 
+// Define E-stop Port
+#define ESTOP 12
+
 /////////////////
+
+
+///// Threads /////
+Thread eSTOP = Thread();
+///////////////////
+
+
+///// Global Variables /////
+bool runMain = true;
+////////////////////////////
+
 
 // E-STOP Function:
 void checkEstop() {
-  
+    Serial.println("Check ESTOP");
+    if(!digitalRead(ESTOP)) {
+      runMain = false;
+      Serial.println("FALSE");
+    }
 }
 
 
@@ -22,16 +43,32 @@ void setup() {
   pinMode(GREEN, OUTPUT);
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT);
+  
   // Initialize output pins:
   digitalWrite(GREEN, HIGH);
   digitalWrite(RED, LOW);
   digitalWrite(YELLOW, LOW);
-  
+
+  // Initialize pin
+  pinMode(ESTOP, INPUT_PULLUP);
+
+  // Initialize Threads
+  eSTOP.onRun(checkEstop);
+  eSTOP.setInterval(500);
 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   analogWrite(GREEN, 50);
 
-}
+  // Checks if ESTOP thread could run:
+  if(eSTOP.shouldRun()) {
+    eSTOP.run();
+  }
+
+  // Gets Estop value:
+  //Serial.println(runMain);
+  
+  }
