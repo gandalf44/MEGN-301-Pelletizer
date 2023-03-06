729ba52190478388d56a314b3b7e93cd3b40a28a
gandalf44 <57021826+gandalf44@users.noreply.github.com>
2023-03-06 11:01:41 -0700

    Updated thread test to properly control pins

--- Test_Code/Nathan/threadding/threadding.ino
+++ Test_Code/Nathan/threadding/threadding.ino
@@ -1,9 +1,37 @@
+
+///// PORTS /////
+
+// Debug LED Ports:
+#define GREEN 9
+#define RED 10
+#define YELLOW 11
+
+/////////////////
+
+// E-STOP Function:
+void checkEstop() {
+  
+}
+
+
+// SETUP:
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // Sets up serial port
+  
+  // Initialize tester pins:
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  // Initialize output pins:
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogWrite(GREEN, 50);
 
 }
