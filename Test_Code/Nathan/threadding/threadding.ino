46f0ee464930718ee874de423c8c20e52136b376
gandalf44 <57021826+gandalf44@users.noreply.github.com>
2023-03-08 10:50:00 -0700

    Working estop

--- Test_Code/Nathan/threadding/threadding.ino
+++ Test_Code/Nathan/threadding/threadding.ino
@@ -4,6 +4,9 @@
 
 ///// PORTS /////
 
+// If DEBUG is defined, more print statements will go to the Serial
+#define DEBUG
+
 // Debug LED Ports:
 #define GREEN 9
 #define RED 10
@@ -19,21 +22,44 @@
 Thread eSTOP = Thread();
 ///////////////////
 
-
 ///// Global Variables /////
 bool runMain = true;
 ////////////////////////////
 
+-----///// Functions /////------
+
+// Debug print function
+void debug_print(String output) {
+  #ifdef DEBUG
+    Serial.print(output);
+  #endif
+}
+
+// debug println function
+void debug_println(String output) {
+  #ifdef DEBUG
+    Serial.println(output);
+  #endif
+}
 
 // E-STOP Function:
 void checkEstop() {
-    Serial.println("Check ESTOP");
+    debug_println("Check ESTOP");
     if(!digitalRead(ESTOP)) {
       runMain = false;
-      Serial.println("FALSE");
+      debug_println("FALSE");
+      killAll();
     }
 }
 
+// shutdown function
+void killAll() {
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+}
+
+-----/////////////////////------
 
 // SETUP:
 void setup() {
@@ -60,15 +86,18 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(GREEN, 50);
 
-  // Checks if ESTOP thread could run:
-  if(eSTOP.shouldRun()) {
-    eSTOP.run();
-  }
-
-  // Gets Estop value:
-  //Serial.println(runMain);
+  while(runMain) {
+    
+    // put your main code here, to run repeatedly:
+    analogWrite(GREEN, 50);
   
+    // Checks if ESTOP thread could run:
+    if(eSTOP.shouldRun()) { // (NOTE: Consider adding a thread controller at a later date)
+      eSTOP.run();
+    }
   }
+
+  // Insert restart code
+  
+}
